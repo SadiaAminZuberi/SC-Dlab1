bcf24b696e213f5f0644f6caa881fa84df1e7b6c
SadiaAminZuberi <sadiaaminzuberi@gmail.com>
2025-10-22 09:54:50 +0500

    Update lab1task.java

--- lab1task.java
+++ lab1task.java
@@ -2,14 +2,14 @@ public class lab1task {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        int a = 10;
-        int b = 20;
+        int a = 30;
+        int b = 40;
         int sum = add(a, b);
 
         System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
-
     public static int add(int x, int y) {
         return x + y;
     }
 }
+
